4debce2a8ad168d9cbc340139c67c70ebe9ce099
Wesley Hanson <wrh1600@gmail.com>
2018-05-05 23:14:49 -0700

    working on event-api-routes.js and event-api.js

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -9,44 +9,4 @@ $(document).ready(function () {
     });
 });
 
-$(".findEvent").on("click", function(event){
-    getEvents();
-});        
 
-
-function getEvents() {
-    $("#view").empty();
-    $.get("/api/events", function(data){
-        console.log(data);
-        for(var x = 0; x < data.length; x++){
-            var title = (data[x].title);
-            var image; 
-            if(data[x].image){
-            image = (data[x].image.thumb.url);
-            }else{
-            image = "http://via.placeholder.com/48x48"
-            }
-            var mTime = (data[x].start_time);
-            var venue = (data[x].venue_name);
-            var venueAdress = (data[x].venue_address);
-            var description = (data[x].description);
-            function convert(input) {
-            return moment(input).format('dddd, MMMM Do YYYY @ h:mm A');
-            }
-            var sTime = convert(mTime);
-            $("#view").append("<h3>" + title + "</h3>" + "<br>");
-            var imageHTML = $('<img>');
-            $(imageHTML).attr("src", image);
-            $("#view").append(imageHTML);
-            $("#view").append("<br>" +
-            sTime +
-            "<br>" +
-            venue +
-            "<br>" +
-            venueAdress +
-            "<br>" +
-            "<br>" +
-            "<br>");
-        }
-    })
-}
