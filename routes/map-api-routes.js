dbde87ef0307d792f7378b486968eab91df8cf85
Bri <bherreras23@gmail.com>
2018-05-12 14:02:11 -0700

    Dir Poss

--- routes/map-api-routes.js
+++ routes/map-api-routes.js
@@ -1,1 +1,25 @@
 var db = require("../models");
+var express = require("express");
+var bodyParser = require("body-parser");
+var request = require("request")
+var router = require('express').Router();
+var PORT = process.env.PORT || 8080;
+var app = express();
+
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
+var apiKey = 'AIzaSyCnzqPNGmrowxNR - tx0qp0KKVG8pCk_g4U';
+
+// turn by turn directions
+request("https://maps.googleapis.com/maps/api/directions/json?origin=Tucson&destination=Phoenix&key=" + apiKey, function (err, res, bod) {
+	var inst = JSON.parse(bod).routes[0].legs[0];
+	//console.log(inst);
+	console.log(inst.start_address);
+	console.log(inst.duration);
+	console.log(inst.end_address);
+	for (var i = 0; i < inst.steps.length; i++) {
+		console.log(inst.steps[i].html_instructions);
+	}
+
+})
+
--- routes/place-api-routes.js
+++ routes/place-api-routes.js
@@ -6,22 +6,22 @@ var router = require('express').Router();
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
-router.get('/api/places', function(req, res, next){
+router.get('/api/places', function (req, res, next) {
 
 	var apiKey = 'AIzaSyCnzqPNGmrowxNR - tx0qp0KKVG8pCk_g4U';
 	var city = req.query.city;
-	
 
-		request("https://maps.googleapis.com/maps/api/geocode/json?address=" + city + "&key=" + apiKey, function(err, response, data){
-			var places = JSON.parse(data);
-			var lat = (places.results[0].geometry.location.lat);
-			var lng = (places.results[0].geometry.location.lng);
-			console.log(lat);
-			console.log(lng);
-			// var userInput = "bar";
-			
-		
-			request("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + lng + "&radius=5500&type=bar&key=" + apiKey, function (err, response, data) {
+
+	request("https://maps.googleapis.com/maps/api/geocode/json?address=" + city + "&key=" + apiKey, function (err, response, data) {
+		var places = JSON.parse(data);
+		var lat = (places.results[0].geometry.location.lat);
+		var lng = (places.results[0].geometry.location.lng);
+		console.log(lat);
+		console.log(lng);
+		// var userInput = "bar";
+
+
+		request("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + lng + "&radius=5500&type=bar&key=" + apiKey, function (err, response, data) {
 			//JSON.parse(data).results[i].name
 			var placeList = JSON.parse(data).results;
 			var randomPlace = [];
@@ -29,18 +29,18 @@ router.get('/api/places', function(req, res, next){
 			var places;
 			for (var i = 0; i < 5; i += 0) {
 				//console.log(err || data);
-				var num = Math.floor(Math.random()*placeList.length);
+				var num = Math.floor(Math.random() * placeList.length);
 				console.log(num);
-				if(!chosen.includes(num)){
+				if (!chosen.includes(num)) {
 					chosen.push(num);
 					places = placeList[num];
 					randomPlace.push(places);
 					i++;
 				}
-				if(randomPlace.length === placeList.length){
+				if (randomPlace.length === placeList.length) {
 					i = 5;
 				}
-				console.log(chosen);	
+				console.log(chosen);
 			}
 			res.json(randomPlace);
 		});
@@ -48,12 +48,12 @@ router.get('/api/places', function(req, res, next){
 
 	})
 
-	
-
-	
 
 
-});	
+
+
+
+});
 
 module.exports = router;
 
