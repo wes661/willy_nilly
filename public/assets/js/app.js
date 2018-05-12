e6ae9620d542d887916b3f729d732a92af03a5cf
Corey Richard <ullrtheglorious@gmail.com>
2018-05-12 14:20:22 -0700

    updated current

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -17,9 +17,21 @@ $(document).ready(function () {
             .addClass('toggle--moving');
 
         setTimeout(function () {
-            $(toggle).removeClass('toggle--moving');
+            $(toggle).removeClass('toggle--moving');    
         }, 200)
     });
+
+});
+var map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 32.253460, lng: -110.911789 },
+        zoom: 8
+    });
+}
+document.querySelector('#mapsModal').addEventListener('click', function () {
+    initMap();
 });
 
 document.querySelector('.img__btn').addEventListener('click', function () {
