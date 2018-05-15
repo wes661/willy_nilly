a24155cbad9177ad710d4688c8d98e7d2ea59928
Bri <bherreras23@gmail.com>
2018-05-14 20:28:50 -0700

    Newest

--- public/assets/js/event-api.js
+++ public/assets/js/event-api.js
@@ -1,5 +1,6 @@
 
 
+
 $(".findEvent").click(function () {
     if ($(".toggle").hasClass("toggle--events")) {
         var searchTerms = {
@@ -10,42 +11,37 @@ $(".findEvent").click(function () {
         $("#view").empty();
         $.get("/api/events", searchTerms, function (data) {
             console.log(data);
+            var mapsTitle;
             if (data.length === 0) {
                 $("#view").html("No results found")
             } else {
                 for (var x = 0; x < data.length; x++) {
                     var title = (data[x].title);
+
                     var image;
-                    if (data[x].image) {
-                        image = (data[x].image.thumb.url);
-                    } else {
-                        image = "http://via.placeholder.com/48x48"
-                    }
+                    var map = "<a href=# data-toggle=modal class=mapsLink data-target=#mapsModal>Get Map</a>";
+                    var sTime = convert(mTime);
                     var mTime = (data[x].start_time);
                     var venue = (data[x].venue_name);
                     var venueAdress = (data[x].venue_address);
                     var description = (data[x].description);
+                    var eventButton = $("<div class=views>");
                     function convert(input) {
                         return moment(input).format('dddd, MMMM Do YYYY @ h:mm A');
                     }
-                    var sTime = convert(mTime);
-                    $("#view").append("<h3>" + title + "</h3>" + "<br>");
-                    var imageHTML = $('<img>');
-                    $(imageHTML).attr("src", image);
-                    $("#view").append(imageHTML);
-                    $("#view").append("<br>" +
-                        sTime +
-                        "<br>" +
-                        venue +
-                        "<br>" +
-                        venueAdress +
-                        "<br>" +
-                        "<br>" +
-                        "<hr>");
+                    eventButton.append("<h1>" + title + "</h1>");
+                    eventButton.append("<br>" + sTime + "<br>" + venue + "<br>" + venueAdress + "<br>");
+                    eventButton.append("<hr>")
+                    eventButton.append(map);
+
+
+
+                    $("#view").append(eventButton);
                 }
+
             }
+
         });
     }
 
 })
-
--- public/assets/js/place-api.js
+++ public/assets/js/place-api.js
@@ -1,31 +1,36 @@
 
 
 
-$(".findEvent").click(function(){
-    if($(".toggle").hasClass("toggle--food")){
+$(".findEvent").click(function () {
+    if ($(".toggle").hasClass("toggle--food")) {
         var searchTerms = {
-            city : $("#location").val(),
+            city: $("#location").val(),
         }
 
         $("#view").empty();
-        $.get("/api/places", searchTerms, function(data){
+        $.get("/api/places", searchTerms, function (data) {
             console.log(data);
-            if(data.length === 0){
+            if (data.length === 0) {
                 $("#view").html("No results found")
-            }else{
-                for(var x = 0; x < data.length; x++){
-                   var name = data[x].name;
-                   var address = data[x].vicinity;
+            } else {
+                for (var x = 0; x < data.length; x++) {
+                    var name = data[x].name;
+                    var address = data[x].vicinity;
+                    var map = "<a href=# data-toggle=modal data-target=#mapsModal>Get Map</a>";
+                    var eventButton = $("<div class=views>");
+                    eventButton.append("<h3>" + name + "</h3>" + "<br>" + address + "<br>");
+                    eventButton.append("<hr>")
+                    eventButton.append(map);
 
-                   $("#view").append("<h3>" + name + "</h3>" + "<br>" 
-                   + address 
-                   + "<br>"
-                   + "<br>"
-                   + "<br>"
-                   + "<br>");
+                    $("#view").append("<h3>" + name + "</h3>" + "<br>"
+                        + address
+                        + "<br>"
+                        + "<br>"
+                        + "<br>"
+                        + "<br>");
                 }
             }
         })
     }
-});    
-  
+});
+
